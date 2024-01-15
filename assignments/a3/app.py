8e0a35f83f079f06d51226ab72a745be47e8edba
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-01-15 13:22:20 +0100

    Req for 4 almost done

--- assignments/a3/app.py
+++ assignments/a3/app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 from database.api import api
 
@@ -17,6 +17,22 @@ app = create_app()
 def homepage():
     data = requests.get('http://127.0.0.1:5000/api/data').json()
     return render_template('index.html', result=data)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = requests.post('http://127.0.0.1:5000/api/data', json={
+        'category': request.form['category'],
+        'data1': request.form['data1'],
+        'data2': request.form['data2']
+    }).json()
+    return data
+
+
+@app.route('/delete/<int:record_id>', methods=['POST'])
+def delete(record_id):
+    data = requests.delete('http://127.0.0.1:5000/api/data/' + str(record_id)).json()
+    return data
 
 
 if __name__ == '__main__':
